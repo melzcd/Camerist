a05649b22462b269e23b082153c8dd83f5d625ba
melzcd <devilzcd@gmail.com>
2015-03-29 19:37:18 +0300

    Переписана функция pop()

--- Camerist.py
+++ Camerist.py
@@ -1,13 +1,15 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 import os
 import sys
 import traceback
 import shutil
 from easygui import *
 
-softVersion = 'Версия программы - 0.0.8'
+softVersion = 'Версия программы - 0.1.1'
 
 class scanDir:
+    # Новый метод возвращает только список файлов
+    # Данный метод взят с сайта http://stackoverflow.com/questions/11968976/python-list-files-in-the-current-directory-only
     # pathScan - Где сканировать
     # fileList - Возвращает список фалов
     def scanDirOnlyFile(self,pathScan):
@@ -33,26 +35,32 @@ class scanDir:
     def copyFile(self,fileList,fromCopy,new_path):
         try:
             for y in fileList:
-                print y.encode('utf-8') #Для теста
+                print y
                 shutil.copy(fromCopy+'\\'+y,new_path)
         except :
             exceptionbox()
         else:
             msgbox(msg= new_path, title = 'Файлы перемещены в ' )
 
-    # Перемещаем файлы
-    # fileList - Список файлов которые надо копировать
-    # fromCopy - Откуда копируем файлы
-    # new_path - Куда перемещаем файлы
-    def moveFile(self,fileList,fromCopy,new_path):
-        try:
-            for y in fileList:
-                print y
-                shutil.move(fromCopy+'\\'+y,new_path)
-        except :
-            exceptionbox()
-        else:
-            msgbox(msg= new_path, title = 'Файлы перемещены в ' )
+#Функция обрезает и изменения формата для копирования.
+##def pop(filelist):
+##    namesFile = []
+##    newFiles = []
+##    for x in filelist:
+##        namesFile.append(x[:-4]) # для .JPG нужно ставить '-4' # для .JEPG нужно ставить '-5'
+##    for y in namesFile:
+##        newFiles.append(y+'.NEF') # Зависит от формата "фотика"
+##    return newFiles
+# Функция немного переписаны, но суть осталась не изменна. Теперь не надо ставить цифры '-4' или '-5' для форматов jpeg и jpg
+def pop(filelist):
+    namesFile = []
+    newFiles = []
+    for x in filelist:
+        namesFile.append(x.rstrip('.jpg') and x.rstrip('.jpeg'))
+    for y in namesFile:
+        newFiles.append(y+'.NEF') # Зависит от формата "фотика"
+    return newFiles
+
 def main():
     while True:
         msg = ""
@@ -70,24 +78,21 @@ def main():
                     break
                 else:
                     try:
+                        # Скануируем папку для получение список файлов.
+                        pathScan = diropenbox(msg ='Скануируем папку для получение список файлов (Оригинальные файлы)')
+
                         # Где создать папку
                         creatrPathDst = diropenbox(msg ='Где создать папку?')
-                        if creatrPathDst == None:
-                            break
-                        # Скануируем папку для получение список файлов.
-                        pathScan = diropenbox(msg ='Скануируем папку для получение список файлов (Обработанные файлы)')
-                        if pathScan == None:
-                            break
+
                         # Откуда копируем
-                        fromCopy = diropenbox(msg ='Откуда копируем файлы (Оригинальные файлы)')
-                        if fromCopy == None:
-                            break
-                        #Запуск процесса
+                        fromCopy = diropenbox(msg ='Откуда копируем файлы (Обработанные файлы)')
+                        #
                         if ynbox(msg='Запустить процесс?',choices=('[<F1>]Yes', '[<F2>]No')):
                             s=scanDir()
                             fileScan = s.scanDirOnlyFile(pathScan)
                             newFolder = s.createrFolder(creatrPathDst,nameFolder)
-                            copyFile = s.copyFile(fileScan,fromCopy,newFolder)
+                            copyFile = s.copyFile(pop(fileScan),fromCopy,newFolder)
+                            #msgbox(msg=copyFile,title = 'Имена файлов')
                     except :
                         exceptionbox ()
                     break
